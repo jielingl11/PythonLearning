4d604240cea6218c31b1cc3088ac8111b005933b
lielingl11 <jacqueline891008@gmail.com>
2020-07-09 16:03:31 +0800

    add Lesson 1 to 3

---
+++ L2_datatype.py
@@ -0,0 +1,28 @@
+# 資料:程式的基本單位
+# 數字
+3456
+3.5 
+# 字串
+"測試中文"
+" Hello World"
+# 布林值
+True 
+False 
+# List 
+[3,4,5]
+["hello","world"]
+#Tuple
+(3,4,5)
+("hello","world")
+# 集合 (set)
+{3,4,5}
+{"hello","world"}
+# 字典
+{"apple":"蘋果","data":"資料"}
+# 變數:用來儲存資料的自訂名稱
+# 變數名稱=資料
+x=3
+# print (資料)
+print (x)
+x=True #取代舊的資料
+print (True)
---
+++ L3_nemberstring.py
@@ -0,0 +1,19 @@
+#數字運算
+x=2+3
+print (x)
+x+=1 #將變數中的數字+1 相當於x=x+1
+
+print(x)
+#字串運算
+s="Hello"
+t='Hell"o'
+v="Hello" "World"
+u="Hello\nWorld"
+w="""Hello
+World"""
+x="Hello"*3+"World"
+#字串會對內部的字元編號，從0開始算起
+print(s[1])
+print(s[1:4]) # 包含開頭的編號，不包含結尾
+print(s[:4]) #從開頭算起
+print(s[1:]) #不給結尾
